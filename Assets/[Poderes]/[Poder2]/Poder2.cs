2d340cb0e999b6b0e9f45d9ef212ef0fe11a22ea
Marc <dlcmarc32@gmail.com>
2023-05-02 00:17:58 +0200

    poder 2 seguramente acabao

--- Assets/[Poderes]/[Poder2]/Poder2.cs
+++ Assets/[Poderes]/[Poder2]/Poder2.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Poder2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float poder2Cooldown;
+    public float range;
+    private float nextTime = 0;
+    public float damage;
+    AttributesEnemies attributesEnemies;
 
-    // Update is called once per frame
+    public Image poder2;
+    public Image cpoder2;
+
+
+    private void Start()
+    {
+        cpoder2.enabled = enabled;
+        poder2.enabled = enabled;
+        poder2.fillAmount = 1;
+    }
     void Update()
     {
-        
+        if (Time.time > nextTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+                foreach (var hitCollider in hitColliders)
+                {
+                    if (hitCollider.transform.gameObject.layer == 7)
+                    {
+                        attributesEnemies = hitCollider.gameObject.GetComponent<AttributesEnemies>();
+                        attributesEnemies.TakeDamage(damage);
+                    }
+                }
+                nextTime = Time.time + poder2Cooldown;
+                poder2.fillAmount = 0;
+            }
+        }
+        else
+        {
+            poder2.fillAmount += 1 / poder2Cooldown * Time.deltaTime;
+            if (poder2.fillAmount >= 1)
+            {
+                poder2.fillAmount = 1;
+            }
+        }
+
     }
 }
